9af4842df2abce010079afff6017bd288af636c0
ajcubeta <ajcubeta@gmail.com>
2011-09-15 21:56:48 +0800

    Remove README 1st commits

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bddfortestersanddevelopers::Application.config.secret_token = '226c19065955054bfd7a5f96b473832aebe893afb86f81344bc5e48ee5594c0c7199295aaad349e04e0fde6be0f5daf03e309a6b5c59a8ac7f1181bfaad8df41'
